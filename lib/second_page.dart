0c2a3fb731c78969820640dc06f6f68d25d7751b
Ameen s <ameenofficial33@gmail.com>
2026-01-02 18:06:04 +0530

    second commit

---
+++ lib/second_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      child: const Text(
+        'This is the second page.',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
